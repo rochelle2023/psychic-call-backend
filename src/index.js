b56e98bce5490892876a2c8ed34b03203600b954
rochelle2023 <149205229+rochelle2023@users.noreply.github.com>
2025-12-28 10:40:16 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const fetch = (...args) =>
+  import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
